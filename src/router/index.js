55e1ed560454e3f07c7e67e10b73faef715cb7a4
katheri0 <singularityqoo.ooq@gmail.com>
2025-08-05 11:21:06 +0300

    feat(edit): add edit page and edit job functionality (new component ) using mock data (jobs.json)
    Course complete. I will improve the project myself.

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import jobsView from "@/views/jobsView.vue";
 import notFoundView from "@/views/notFoundView.vue";
 import jobView from "@/views/jobView.vue";
 import AddJobView from "@/views/addJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 const router = createRouter(
     {
         history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter(
                     path: '/jobs/:id',
                     name: 'job',
                     component: jobView,
+                },
+                {
+                    path: '/edit/:id',
+                    name: 'edit-job',
+                    component: EditJobView,
                 },
                 {
                     path: '/:catchAll(.*)',
