e1718aafa2ab70c7f4f490b7e71090e58014f025
katheri0 <singularityqoo.ooq@gmail.com>
2025-08-24 04:33:03 +0300

    fix(navbar) hide in home

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
 import navebar from '@/components/navebar.vue';
 import { RouterView } from 'vue-router';
 import BaseComponentpage from './views/BaseComponentpage.vue';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+
 </script>
 <template>
-        <navebar/>
+        <navebar  v-if="!route.meta.hideNavbar" />
         <RouterView/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,14 @@ import BaseComponentpage from "@/views/BaseComponentpage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: "/main", name: "main", component: HomeView },
+    {path: "/main", name: "main", component: HomeView,},
     {path: "/jobs", name: "jobs", component: jobsView },
     {path: "/jobs/add", name: "add-job", component: AddJobView },
     {path: "/jobs/:id", name: "job", component: jobView },
     {path: "/edit/:id", name: "edit-job", component: EditJobView },
     {path: '/login', name: 'login', component: loginPageView, },
     {path: '/register', name: 'register', component: singinPageView,},
-    {path: '/', name: 'home', component: BaseComponentpage,},
+    {path: '/', name: 'home', component: BaseComponentpage, meta: { hideNavbar: true }  },
     {path: "/:catchAll(.*)", name: "not-found", component: notFoundView },
   ],
 });
